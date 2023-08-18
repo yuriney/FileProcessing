c09a4c781647edbbe9d66f5f4e17302b9e24543d
Yuri Nunes <yurimn14@outlook.com>
2023-08-18 15:28:22 -0300

    I still have some bugs with outcomes but it's looking better, this is the start

---
+++ src/main/java/org/example/FileProcessing.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileProcessing {
+    public static void main(String[] args) {
+        String inputFile = "C:\\Users\\icabi\\Documents\\MP - DOCS\\Automações e Scripts\\Test\\Splitting\\am_edicao.txt";
+
+        try {
+            List<String> paths = readPathsFromFile(inputFile);
+
+            List<String> programaOutcomes = new ArrayList<>();
+            List<String> fileNameOutcomes = new ArrayList<>();
+            List<String> extensionFileOutcomes = new ArrayList<>();
+            List<String> pathOutcomes = new ArrayList<>();
+
+            for (String path : paths) {
+                String[] parts = path.split("\\\\");
+                String fullPath = parts[parts.length - 1];
+
+                String[] nameAndExtension = fullPath.split("\\.");
+                String folder = parts[0];
+                String fileName = nameAndExtension[0];
+                String extension = nameAndExtension.length > 1 ? nameAndExtension[1] : "";
+                String uppercasePath = path.replaceAll("[^a-zA-Z0-9\\\\]", "").toUpperCase();
+
+                programaOutcomes.add(folder);
+                fileNameOutcomes.add(fileName);
+                extensionFileOutcomes.add(extension);
+                pathOutcomes.add(uppercasePath);
+            }
+
+            writeOutcomeToFile("C:\\Users\\icabi\\Documents\\MP - DOCS\\Automações e Scripts\\Test\\Splitting\\programa.txt", programaOutcomes);
+            writeOutcomeToFile("C:\\Users\\icabi\\Documents\\MP - DOCS\\Automações e Scripts\\Test\\Splitting\\file_name.txt", fileNameOutcomes);
+            writeOutcomeToFile("C:\\Users\\icabi\\Documents\\MP - DOCS\\Automações e Scripts\\Test\\Splitting\\extension_file.txt", extensionFileOutcomes);
+            writeOutcomeToFile("C:\\Users\\icabi\\Documents\\MP - DOCS\\Automações e Scripts\\Test\\Splitting\\path.txt", pathOutcomes);
+
+            System.out.println("Outcome files generated successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> readPathsFromFile(String filePath) throws IOException {
+        List<String> paths = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            paths.add(line.trim());
+        }
+
+        reader.close();
+        return paths;
+    }
+
+    private static void writeOutcomeToFile(String filePath, List<String> outcomes) throws IOException {
+        FileWriter writer = new FileWriter(filePath);
+        for (String outcome : outcomes) {
+            writer.write(outcome + "\n");
+        }
+        writer.close();
+    }
+}
